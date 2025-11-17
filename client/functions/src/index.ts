363b641b91983799cc8111799c90c12819f94150
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2025-11-17 22:08:06 +0800

    refactor(index.ts): clean up exports by removing unused functions and organizing code

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -10,31 +10,33 @@
 import { setGlobalOptions } from "firebase-functions";
 
 // Import our functions
-export { helloWorld } from "./hello-world";
 export { sendVerificationEmail } from "./send-verification-email";
 export { verifyEmail } from "./verify-email";
-export {
-  onTypeScriptFunctionUpdated,
-  onTypeScriptFunctionUpdatedSimple,
-} from "./recompute-on-function-update";
-export { testRecompute } from "./test-recompute";
+
 export { generateReservationEmail } from "./generate-reservation-email";
 export { sendReservationEmail } from "./send-reservation-email";
 export { generateCancellationEmail } from "./generate-cancellation-email";
 export { sendCancellationEmail } from "./send-cancellation-email";
 export { getDraftSubject } from "./get-draft-subject";
 export { getEmailDetails } from "./get-email-details";
-// export { telegramBot } from "./telegram-bot";
-
+// Export email utilities
+export { EmailTemplateLoader } from "./email-template-loader";
 // Export only the scheduled email processor (cron job)
 // Other email functions have been migrated to Next.js API routes
 export { processScheduledEmails } from "./scheduled-emails";
-
 // Export payment reminder trigger
 export { onPaymentReminderEnabled } from "./payment-reminder-trigger";
 
-// Export email utilities
-export { EmailTemplateLoader } from "./email-template-loader";
+
+
+// export { telegramBot } from "./telegram-bot";
+// export {
+//   onTypeScriptFunctionUpdated,
+//   onTypeScriptFunctionUpdatedSimple,
+// } from "./recompute-on-function-update";
+// export { testRecompute } from "./test-recompute";
+
+
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
